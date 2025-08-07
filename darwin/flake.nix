f27680707c0535bead76b2ef0c99d0bc4663829d
Yaacov <yaacov@goodimpact.studio>
2025-08-07 16:08:53 +0200

    Add system flake + homemanager configs

---
+++ darwin/flake.nix
@@ -0,0 +1,264 @@
+{
+  description = "Cool nix-darwin system flake";
+
+  inputs = {
+    # Nixpkgs: collection of software packages for the Nix package manager
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # VSCode extensions
+    # see https://davi.sh/blog/2024/11/nix-vscode/
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+
+    # nix-darwin (MacOS)
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Homebrew
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # NUR
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{
+    self,
+    darwin,
+    nixpkgs,
+    nix-homebrew,
+    homebrew-core,
+    homebrew-cask,
+    home-manager,
+    nix-vscode-extensions,
+    nur,
+    ...
+  }:
+  let
+    configuration = { pkgs, config, ... }: {
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      nixpkgs.config.allowUnfree = true;
+
+      nixpkgs.overlays = [
+        nix-vscode-extensions.overlays.default
+      ];
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment = {
+        variables = {
+          DOCKER_HOST = "unix:///var/folders/j4/pkqvd4dj1qj2tn6b0g9z763w0000gn/T/podman/podman-machine-default-api.sock";
+        };
+        systemPackages = with pkgs; [
+          # beekeeper-studio // doesn't exist for aarch64-darwin
+          bitwarden-desktop
+          brave
+          chezmoi
+          corepack
+          cowsay
+          fortune
+          fzf
+          gh
+          libreoffice-bin
+          mkalias
+          neofetch
+          nur
+          oh-my-posh
+          # signal-desktop // not working 06/05/2025
+          slack
+          zoom-us
+          zsh
+        ];
+      };
+
+      fonts.packages = with pkgs; [
+        nerd-fonts.fira-code
+        nerd-fonts.droid-sans-mono
+        nerd-fonts.meslo-lg
+      ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+          "podman"
+          "podman-compose"
+          "qrencode"
+          "verapdf"
+          "vips"
+          "xpdf"
+        ];
+        casks = [
+          "deepl"
+          "figma"
+          "firefox"
+          "ghostty"
+          "imageoptim"
+          "podman-desktop"
+          "signal"
+          "spotify"
+          "vlc"
+          "whatsapp"
+        ];
+        onActivation.cleanup = "zap";
+        onActivation.upgrade = true;
+      };
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      programs = {
+        ssh = {
+          knownHosts = {
+            "github/ed25519" = {
+              publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+              hostNames = [ "github.com" ];
+            };
+            "github/sha2" = {
+              publicKey = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=";
+              hostNames = [ "github.com" ];
+            };
+            "github/rsa" = {
+              publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=";
+              hostNames = [ "github.com" ];
+            };
+          };
+        };
+      };
+
+      # Enable Touch ID with sudo
+      security.pam.services.sudo_local.touchIdAuth = true;
+
+      system = {
+        # Create aliases instead of symlinks for all installed applications
+        # See https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
+        activationScripts = {
+          applications.text = let
+            env = pkgs.buildEnv {
+              name = "system-applications";
+              paths = config.environment.systemPackages;
+              pathsToLink = "/Applications";
+            };
+          in
+            pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read -r src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+                '';
+
+          postActivation.text = ''
+            # Following line should allow us to avoid a logout/login cycle
+            sudo /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+            '';
+        };
+
+        primaryUser = "yaaax";
+
+        # Set Git commit hash for darwin-version.
+        configurationRevision = self.rev or self.dirtyRev or null;
+
+        # System (MacOS)
+        defaults = {
+          dock.autohide = true;
+          finder = {
+            FXPreferredViewStyle = "clmv";
+          };
+          spaces.spans-displays = true;
+
+          CustomSystemPreferences = {
+            NSGlobalDomain = {
+              # Add a context menu item for showing the Web Inspector in web views
+              WebKitDeveloperExtras = true;
+              "com.apple.mouse.tapBehavior" = 1;
+              "com.apple.swipescrolldirection" = true;
+            };
+          };
+        };
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        stateVersion = 6;
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#macbook
+    darwinConfigurations.macbook = darwin.lib.darwinSystem {
+      modules = [
+        configuration
+
+        # Adds the NUR overlay
+        nur.modules.darwin.default
+
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "yaaax";
+
+            # Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Enable fully-declarative tap management
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+          };
+        }
+
+        home-manager.darwinModules.home-manager
+        {
+          # Mandatory in order to make config build (even if repeated in home.nix)
+          users = {
+            users = {
+              yaaax.home = "/Users/yaaax";
+            };
+          };
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.yaaax = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ darwin/home.nix
@@ -0,0 +1,276 @@
+{ config, pkgs, nur, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    username = "yaaax";
+    homeDirectory = "/Users/yaaax";
+
+    file = {
+      # Ghostty config
+      ".config/ghostty/config".text = ''
+        # Theme
+        theme = "catppuccin-mocha"
+
+        # UI
+        macos-titlebar-style = "tabs"
+        window-colorspace = "display-p3"
+
+        # Keys
+        keybind = global:cmd+grave_accent=toggle_quick_terminal
+
+        # Functionnalities
+        window-save-state = always
+
+        scrollback-limit = 4294967295
+      '';
+
+      # Neovim config
+      ## Configure git commit message highlighting
+      ".config/nvim/after/ftplugin/gitcommit.vim".text = ''
+        highlight gitcommitOverflow guifg=#bf616a ctermfg=202
+      '';
+
+      # SSH config
+      ## Add identity file (private key) to access github.com to
+      ".ssh/config".text = ''
+        Host github.com
+        UseKeychain yes
+        AddKeysToAgent yes
+        IdentityFile ~/.ssh/id_ed25519
+      '';
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs = {
+    google-chrome = {
+      enable = true;
+    };
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    # firefox = {
+    #   enable = true;
+    #   profiles = {
+    #     default = {
+    #       id = 0;
+    #       name = "default";
+    #       extensions = {
+    #         packages = with pkgs.nur.repos.rycee.firefox-addons; [
+    #           bitwarden
+    #           headingsmap
+    #           ublock-origin
+    #           wappalyzer
+    #           web-developer
+    #         ];
+    #       };
+    #     };
+    #   };
+    # };
+
+    git = {
+      aliases = {
+        co = "checkout";
+        oops = "commit --amend --no-edit";
+      };
+      enable = true;
+      extraConfig = {
+        pull.ff = "only";
+        stash.showPatch = true;
+      };
+      userEmail = "yaacov@goodimpact.studio";
+      userName = "Yaacov";
+      delta = {
+        enable = true;
+        options = {
+          dark = true;
+          minus-style = "red bold auto";
+          plus-style = "green bold #114411";
+        };
+      };
+    };
+
+    # modern vim
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+    };
+
+    oh-my-posh = {
+      enableZshIntegration = true;
+    };
+
+    # Spotify
+    spotify-player = {
+      enable = true;
+    };
+
+    # VS Codium
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      profiles = {
+        default = {
+          userSettings = {
+            "[json]" = {
+              "editor.defaultFormatter" = "vscode.json-language-features";
+            };
+            "[jsonc]" = {
+              "editor.defaultFormatter" = "vscode.json-language-features";
+            };
+            "[vue]" = {
+              "editor.defaultFormatter" = "Vue.volar";
+            };
+            "containers.containerClient" = "com.microsoft.visualstudio.containers.podman";
+            "editor.codeActionsOnSave" = {
+              "source.fixAll" = "explicit";
+            };
+            "editor.defaultFormatter" = "dbaeumer.vscode-eslint";
+            "editor.rulers" = [80];
+            "files.associations" = {
+              "*.json" = "jsonc";
+            };
+            "files.autoSave" = "off";
+            "javascript.format.enable" = false;
+            "terminal.integrated.fontFamily" = "MesloLGM Nerd Font";
+            "typescript.preferences.importModuleSpecifier" = "relative";
+            "typescript.tsserver.experimental.enableProjectDiagnostics" = true;
+            "typescript.format.enable" = false;
+          };
+          extensions = with pkgs.vscode-marketplace; [
+            bbenoist.nix
+            dbaeumer.vscode-eslint
+            dreamcatcher45.podmanager
+            eamodio.gitlens
+            esbenp.prettier-vscode
+            github.vscode-github-actions
+            ms-azuretools.vscode-containers
+            ritwickdey.liveserver
+            tamasfe.even-better-toml
+            possan.nbsp-vscode
+          ] ++ [
+            pkgs.vscode-extensions.vue.volar
+          ];
+        };
+      };
+    };
+
+    zsh = {
+      enable = true;
+      syntaxHighlighting = {
+        enable = true; # Amazing oneline!
+      };
+      shellAliases = {
+        docker = "podman";
+
+        l = "ls -al --color";
+
+        g = "git";
+
+        ga = "git add";
+
+        gc = "git commit";
+
+        gd = "git diff";
+
+        gds = "git diff --staged";
+
+        # Best default 'git log':
+        gl = "glog --name-status";
+
+        # Fancy 'git log --graph':
+        glg = "glog --graph";
+
+        # Fancy 'git log --graph --oneline':
+        glgo = "git log -10 --graph --date=format:\"%d/%m/%y\" --pretty=format:\"%C(yellow)%h%Creset   %C(white)%ad%Creset%x09%C(bold)%s%x09%C(bold green)%D%Creset%n\"";
+
+        # Fancy 'git log --graph --stat':
+        glgs = "glog --graph --stat";
+
+        # Fancy 'git log --oneline':
+        glo = "git log -10 --date=format:\"%d/%m/%y\" --pretty=format:\"%C(yellow)%h%Creset   %C(white)%ad%Creset    %C(bold)%s     %C(bold green)%D%Creset\"";
+
+        # Regular 'git log' in style:
+        glog = "git log -10 --date=format:\"%A %B %d %Y at %H:%M\" --pretty=format:\"%C(yellow)%H%Creset%x09%C(bold green)%D%Creset%n%<|(40)%C(white)%ad%x09%an%Creset%n%n    %C(bold)%s%Creset%n%w(0,4,4)%+b\"";
+
+        gp = "git push";
+
+        gs = "git status -s";
+
+        # Regular 'git show' in style:
+        gsh = "git show --date=format:\"%A %B %d %Y at %H:%M\" --pretty=format:\"%C(yellow)%H%Creset%x09%C(bold green)%D%Creset%n%<|(40)%C(white)%ad%x09%an%Creset%n%n    %C(bold)%s%Creset%n%w(0,4,4)%+b%n\"";
+      };
+      initContent = ''
+        # Oh My Posh initialization
+        eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config $HOME/.config/oh-my-posh/zen.toml)"
+      '';
+    };
+  };
+
+  targets.darwin = {
+    currentHostDefaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+
+        InitialKeyRepeat = 14;
+        KeyRepeat = 2;
+
+        # Tap to click on mouse.
+        # ⚠️ Disabled as Not working well…
+        # "com.apple.mouse.tapBehavior" = 1;
+
+        # Normal scrolling.
+        # "com.apple.swipescrolldirection" = false;
+      };
+      "com.apple.controlcenter" = {
+        BatteryShowPercentage = true;
+      };
+      "com.apple.Safari" = {
+        AlwaysRestoreSessionAtLaunch = true;
+        IncludeDevelopMenuPreferenceKey = true;
+        IncludeInternalDebugMenu = true;
+        ShowFullURLInSmartSearchField = true;
+        ShowOverlayStatusBar = true;
+        SuppressSearchSuggestions = true;
+        UniversalSearchEnabled = true;
+        WebKitDeveloperExtrasEnabledPreferenceKey = true;
+        "WebKitPreferences.tabFocusesLinks" = 1;
+      };
+
+      "com.apple.Safari.ContentPageGroupIdentifier" = {
+        WebKit2DeveloperExtrasEnabled = true;
+      };
+
+      "com.apple.Safari.SandboxBroker" = {
+        ShowDevelopMenu = true;
+      };
+    };
+  };
+}
+
+# Some notes
+# ==========
+#
+# Local Apache
+# ------------
+#
+# J’ai suivi [How to Set Up a Local Web Server on macOS 15 Sequoia](https://sausheong.com/how-to-set-up-a-local-web-server-on-macos-15-sequoia-90a70293ce74)
